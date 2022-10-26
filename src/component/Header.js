6dd131827901fa4abf21ea34dd61ab973f03d478
MD  SHABBIR JAMAL <mdshabbir820141@gmail.com>
2022-10-26 13:25:17 +0530

    sticky navbar 1.0

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../style/Header.css'
 function Header() {
-  
+
+  const [color,setColor] = React.useState(false)
+  const changeColor = () => {
+    if(window.scrollY >=60) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  }
+  window.addEventListener('scroll',changeColor)
+
   return (
-    <div className="header">
+    <div className={color ? 'header header-bg' : 'header'}>
       <div className="option">
         <h2 id="logo">MDSJ</h2>
         <a href = "#About"><h4 id="option-About">About</h4></a>
