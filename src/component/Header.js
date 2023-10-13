a4b44b6e6398b31e78fd16c5f9384ffabf69fa70
jamal474 <mdshabbir820141@gmail.com>
2023-10-13 18:44:23 +0530

    Navbar responsive Implemented

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,11 +9,17 @@ function Header() {
     <section className= 'header' >
       <div className="option" name="myElements">
         <h1 className = "logo" id = "logo">jamal474</h1>
-        <Link to="/" className = "nav-option">About</Link>
-        <Link to="/Education"  className = "nav-option">Education</Link>
-        <Link to="/Projects" className = "nav-option">Projects</Link>
-        <Link to="/Skills"  className = "nav-option">Skills</Link>
-        <Link to="/Contact" className = "nav-option">Contacts</Link>
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+        
+          <div className = "nav">
+            <Link to="/" className = "nav-option">About</Link>
+            <Link to="/Education"  className = "nav-option">Education</Link>
+            <Link to="/Projects" className = "nav-option">Projects</Link>
+            <Link to="/Skills"  className = "nav-option">Skills</Link>
+            <Link to="/Contact" className = "nav-option">Contacts</Link>
+          </div>
+       
       </div>
     </section>
   );
