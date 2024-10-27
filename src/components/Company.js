a3a2456aef67374ba0b816415e58ca24ca5046e6
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2024-10-27 17:41:53 +0530

    Experience page addition, data from json

---
+++ src/components/Company.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../style/Company.css'
+
+const Company = (props) => {
+    //making the work details
+    // two arrays are passed
+    // work, project
+    let workItems = props.work.map((designation) => {
+        const projectItems = props.project
+                                    .filter(projectElement => projectElement.roleId === designation.roleId)
+                                    .map(projectElement => (
+                                        <div className = "company-work-detail" key = {projectElement.projectName}>
+                                            <div className = "company-work-name">{projectElement.projectName}</div>
+                                            <ul className = "company-work-list">
+                                                {projectElement.pointers.map((point, index) => (
+                                                    <li className = "company-work-pointer" key = {index}>{point}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    ));
+        return (
+            <div key = {designation.roleId}>
+                <div className = "company-work-role">{designation.role}</div>
+                <div className = "company-work-duration">{designation.duration}</div>
+                {projectItems}
+            </div>
+        );
+    });
+    
+    return (
+        <>
+            <div class="timeline-item">
+                <div class="company-info">
+                    <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems : 'center'}}>
+                        <div className="company-name">{props.name}</div>
+                        <a className = "company-link" href = {props.officialWebsite}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f56969" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                        </a>
+                    </div>
+                    <div className="company-location">{props.location}</div>
+                    <div className="work-duration">{props.duration}</div>
+                </div>
+                <div class="mid-circle shine">
+                    <img className="company-icon" src={props.logo} alt="keywords studios logo" style = {{width : props.logoWidth}}/>
+                </div>
+                <div className = "company-work">{workItems}</div>
+            </div>
+        </>
+    )
+}
+
+export default Company;
---
+++ src/pages/Experience.js
@@ -0,0 +1,44 @@
+import React from "react";
+import SEO from './SEO'
+import '../style/Experience.css'
+import Company from '../components/Company'
+import kwsLogo from '../assets/icon/KWS.svg'
+import glLogo from '../assets/icon/GL.svg'
+import experienceData from '../assets/data/experienceData.json'
+
+function Experience() {
+    const logo = [kwsLogo, glLogo];
+
+    const companyList = experienceData.company.map((companyElement) => {
+
+        const roleList = experienceData.work.filter((role) => role.companyId === companyElement.companyId);
+        const projectList = experienceData.project.filter((project) => roleList.some((role) => role.roleId === project.roleId));
+        return (
+            <Company name = {companyElement.name} 
+                     location = {companyElement.location}
+                     duration = {companyElement.duration}
+                     work = {roleList}
+                     project = {projectList}
+                     logo = {logo[companyElement.companyId - 1]}
+                     logoWidth = {companyElement.logoWidth}
+                     officialWebsite = {companyElement.site}
+            />
+        );
+    })
+    return (
+        <section className="experience" name="myElements" id="experience">
+            <SEO
+                title="Experience - Md Shabbir Jamal"
+                description="Hi, I am a CS undergrad with a strong enthusiasm for web development, machine learning, and competitive programming. Explore my interests on this website."
+                name="@lamajribbahs"
+                image="../assets/image/picture.jpg" />
+            <h2 className="experience-section-title">Experience</h2>
+            <div class="timeline-container">
+                <div class="timeline-line"></div>
+                {companyList}
+            </div>
+        </section>
+    );
+}
+
+export default Experience;  
